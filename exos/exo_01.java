ddb38928d8e4ec925f5e8fb4bb49c65a23fb3d2c
weiss67 <weissj.pro@gmail.com>
2025-06-12 15:13:41 +0200

    Message

--- exos/exo_01.java
+++ exos/exo_01.java
@@ -1,5 +1,8 @@
 
-/*A l'aide de boucle, tableau, exception et fonction. Me faire un tableau contenant 5 noms de ville (Vous pouvez choisir vos propres villes), créer une méthode pour afficher les villes du tableau numéroté par ligne et autre une méthode pour choisir une ville selon 
+/*A l'aide de boucle, tableau, exception et fonction. 
+Me faire un tableau contenant 5 noms de ville (Vous pouvez choisir vos propres villes), 
+créer une méthode pour afficher les villes du tableau numéroté par ligne 
+et autre une méthode pour choisir une ville selon 
 
 la position de sa cellule du tableau et puis affichera un message de bienvenue dans la langue de la ville choisie.:
 
@@ -50,17 +53,33 @@ Votre voyage est terminé :) !
 */
 import java.util.Scanner;
 public class exo_01 {
+
+    public static int lastCity(int[] listCity) {
+        return listCity[listCity.length - 1]; // Retourne la dernière valeur du tableau
+    }
+
+    public static void citiesAll(String[] table_cities) {
+        for (int i = 0; i < table_cities.length; i++) {
+            System.out.println(i + " - " + table_cities[i]); // Affiche chaque valeur du tableau
+        }
+    }
+
+    public static void citySelectionned(int selectionned, String[] table_welcomes) {
+        if(selectionned < 4){
+            System.out.println(table_welcomes[selectionned]);
+        }else{System.out.println("Désolé, cette ville n'est pas sur la liste !");}
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // Déclaration du scanner Instance de l'Objet Scanner 
-
         String v0 = "Paris", v1 = "Londres", v2 = "Madrid", v3 = "Lisbonne", v4 = "Berlin"; boolean relaunch = true;
+        String[] table_cities = {v0, v1, v2, v3, v4};
         String[] table_welcomes = {"Bienvenue à Paris !", "Welcome to London !", "Bienvenido a Madrid !", "Bem-vindo a Lisboa !", "Willkommen in Berlin !"};
-        System.out.println("Bonjour, quelle ville voulez-vous visiter ?\n0- "+v0+"\n1- "+v1+"\n2 - "+v2+"\n3 - "+v3+"\n4 - "+v4);
+        System.out.println("Bonjour, quelle ville voulez-vous visiter ?");
+        citiesAll(table_cities);
         while(relaunch){
             int selectionned = sc.nextInt();
-            if(selectionned < 4){
-                System.out.println(table_welcomes[selectionned]);
-            }else{System.out.println("Désolé, cette ville n'est pas sur la liste !");}
+            citySelectionned(selectionned, table_welcomes);
             System.out.println("Voulez-vous visiter une autre ville ? (false/true)");
             relaunch = sc.nextBoolean();
         }
